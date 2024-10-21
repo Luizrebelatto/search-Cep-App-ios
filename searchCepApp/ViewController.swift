4c91bd18392a3ab413d2b681a937d4354a3128df
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2024-10-21 12:34:07 -0300

    build: init project
    - init project
    - prepare app to use uikit without storyboard

---
+++ searchCepApp/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  searchCepApp
+//
+//  Created by Luiz Gabriel Rebelatto Bianchi on 21/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
